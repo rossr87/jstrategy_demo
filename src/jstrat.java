dda513db9240fb3937b71023b826dbb0fa384fc3
rossr87 <singularity.imminent@gmail.com>
2017-11-27 23:51:44 +0000

    Basic Strategy Pattern and some Content

--- src/jstrat.java
+++ src/jstrat.java
@@ -2,8 +2,14 @@
 public class jstrat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		GenStyle genstyle = GenStyle.HYDRO;
+		PowerPlant motherboard = new PowerPlant(genstyle);
+		
+		System.out.println(motherboard.get_genstyle());
+		motherboard.set_gen_style_strategy(GenStyle.NUCLEAR);
+		System.out.println(motherboard.get_genstyle());
 	}
 
 }
